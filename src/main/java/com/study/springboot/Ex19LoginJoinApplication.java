2b1505901fc436798676d892226790f68e095148
psk910903 <psk910903@gmail.com>
2023-04-13 15:42:19 +0900

    0413

--- src/main/java/com/study/springboot/Ex19LoginJoinApplication.java
+++ src/main/java/com/study/springboot/Ex19LoginJoinApplication.java
@@ -2,12 +2,16 @@ package com.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class Ex19LoginJoinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ex19LoginJoinApplication.class, args);
+		//SpringApplication.run(Ex19LoginJoinApplication.class, args);
+		SpringApplication application = new SpringApplication(Ex19LoginJoinApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
