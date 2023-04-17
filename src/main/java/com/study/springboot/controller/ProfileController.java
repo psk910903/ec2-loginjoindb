725573a19436205f43695110940b9cf55889dc87
psk910903 <psk910903@gmail.com>
2023-04-17 13:02:54 +0900

    0417

--- src/main/java/com/study/springboot/controller/ProfileController.java
+++ src/main/java/com/study/springboot/controller/ProfileController.java
@@ -3,6 +3,7 @@ package com.study.springboot.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
