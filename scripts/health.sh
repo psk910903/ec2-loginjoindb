ae283dd54a4747cc079469e611019e00cafec3f6
psk910903 <psk910903@gmail.com>
2023-04-17 12:27:04 +0900

    commit

--- scripts/health.sh
+++ scripts/health.sh
@@ -17,6 +17,8 @@ do
  RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
+echo "> $UP_COUNT"
+
  if [ ${UP_COUNT} -ge 1 ]
  then #$up_count >= 1 ("real" 문자열이 있는지 검증)
    echo "> Health check 성공"
